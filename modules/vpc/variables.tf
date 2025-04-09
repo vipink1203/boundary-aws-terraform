dd4332d91b12df079ce19e36402e9b28fd5bb043
Vipin Kumar V <vipink1203@gmail.com>
2025-04-09 11:17:39 -0400

    Add VPC module variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,30 @@
+variable "name" {
+  description = "Name to be used for the VPC and related resources"
+  type        = string
+}
+
+variable "cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  type        = list(string)
+}
+
+variable "common_tags" {
+  description = "Common tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
