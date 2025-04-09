d3a5c9d975a8e1a0a7fae90fe31c2c321669eb87
Vipin Kumar V <vipink1203@gmail.com>
2025-04-09 11:27:58 -0400

    Add load balancer module variables.tf

---
+++ modules/load_balancer/variables.tf
@@ -0,0 +1,30 @@
+variable "name" {
+  description = "Name for the load balancer and related resources"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs for the load balancer"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "ID of the security group for the load balancer"
+  type        = string
+}
+
+variable "controller_instance_id" {
+  description = "ID of the Boundary controller EC2 instance"
+  type        = string
+}
+
+variable "worker_instance_id" {
+  description = "ID of the Boundary worker EC2 instance"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Common tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
